6d6b660eb7fde8a76b64c74aed527170c75a2045
Erfan-Zr <lockelerz@gmail.com>
2024-12-06 08:39:02 +0330

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'medication-tracker',
+    // implementing lazy loading for faster initial load and better user experience
+    loadChildren: () =>
+      import('./pages/medication-tracking/medication-tracking.module').then(
+        (m) => m.MedicationTrackingModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
